09f265ea9a14ed29365f45e75697008b2bd68b9e
viral-sabhaya <113441764+Viral-Sabhaya@users.noreply.github.com>
2024-10-08 22:30:55 +0530

    feat: Colorize treminal

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -5,23 +5,37 @@ import config from '../config/config';
 import { EApplicationEnvironment } from '../constant/application';
 import path from 'path';
 import * as sourceMapSupport from 'source-map-support';
-
+import { blue, green, red, yellow, magenta } from 'colorette'
 // Linking Trace support
 sourceMapSupport.install();
+
+const colorizeLevel = (level: string) => {
+  switch (level) {
+    case 'ERROR':
+      return red(level)
+    case 'info':
+      return blue(level)
+    case 'WARN':
+      return yellow(level)
+    default:
+      return level
+  }
+}
 
 const consoleLogFormate = format.printf((info) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { level, message, timestamp, meta = {} } = info
-  const customLevel = level.toLowerCase();
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const customTimeStamp = timestamp;
+  const customLevel = colorizeLevel(level.toLowerCase());
+
+  const customTimeStamp = green(timestamp as string);
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const customMessage = message;
   const customMeta = util.inspect(meta, {
     showHidden: false,
-    depth: null
+    depth: null,
+    colors: true
   })
-  const customLog = `${customLevel} [${customTimeStamp}] ${customMessage}\n${'META'} ${customMeta}\n`
+  const customLog = `${customLevel} [${customTimeStamp}] ${customMessage}\n${magenta('META')} ${customMeta}\n`
   return customLog
 })
 
